4670a069b3f82e51eae12748b770a01e6d67e9f7
liarcrown <liarcrown@naver.com>
2024-08-20 16:30:47 +0900

    feat : Add Search Module #2

--- Application/Reward/src/main/java/com/example/Reward/Advertisement/Controller/AdController.java
+++ Application/Reward/src/main/java/com/example/Reward/Advertisement/Controller/AdController.java
@@ -1,33 +1,33 @@
-package com.example.Reward.Advertisement.Controller;
-
-
-import io.swagger.v3.oas.annotations.Operation;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/ad")
-public class AdController {
-
-
-    @GetMapping("/info")
-    @Operation(description = "광고 의뢰한 기업들의 목록")
-    public ResponseEntity<?> getInfo(){
-
-    }
-
-    @GetMapping("/{media_id}")
-    @Operation(description = "광고 영상 uri")
-    public ResponseEntity<?> getAdUrl(){
-
-    }
-
-    @GetMapping("/{media_id}/quiz")
-    @Operation(description = "quiz ")
-
-    @PostMapping("/{media_id}/quiz")
-    @GetMapping("/detail")
-}
+//package com.example.Reward.Advertisement.Controller;
+//
+//
+//import io.swagger.v3.oas.annotations.Operation;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/ad")
+//public class AdController {
+//
+//
+//    @GetMapping("/info")
+//    @Operation(description = "광고 의뢰한 기업들의 목록")
+//    public ResponseEntity<?> getInfo(){
+//
+//    }
+//
+//    @GetMapping("/{media_id}")
+//    @Operation(description = "광고 영상 uri")
+//    public ResponseEntity<?> getAdUrl(){
+//
+//    }
+//
+//    @GetMapping("/{media_id}/quiz")
+//    @Operation(description = "quiz ")
+//
+//    @PostMapping("/{media_id}/quiz")
+//    @GetMapping("/detail")
+//}
