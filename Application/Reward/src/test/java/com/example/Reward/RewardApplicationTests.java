3e10786fccbbf5923b409f62f8623447955c7f77
lemonticsoul <sjho714@naver.com>
2024-08-26 15:57:12 +0900

    fix:ec2 build.gradle 오류 해결
    #4

--- Application/Reward/src/test/java/com/example/Reward/RewardApplicationTests.java
+++ Application/Reward/src/test/java/com/example/Reward/RewardApplicationTests.java
@@ -6,9 +6,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class RewardApplicationTests {
 
-	@Test
+
 	void contextLoads() {
-		System.out.println(Math.round((150.0 / 75000) * 1000000) / 1000000.0);
+
 	}
 
 }
