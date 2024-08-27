2105fdb7f5757a8c975ade8948eb9a83f64a731c
lemonticsoul <sjho714@naver.com>
2024-08-27 22:20:50 +0900

    fix: gitgnore 바꿈
    #4

--- Application/Reward/src/main/java/com/example/Reward/Advertisement/Webclient/TokenInfo.java
+++ Application/Reward/src/main/java/com/example/Reward/Advertisement/Webclient/TokenInfo.java
@@ -3,6 +3,7 @@ package com.example.Reward.Advertisement.Webclient;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -19,5 +20,6 @@ public class TokenInfo {
     @GeneratedValue
     private Long id;
 
+    @Size(max=10000)
     private String accessToken;
 }
