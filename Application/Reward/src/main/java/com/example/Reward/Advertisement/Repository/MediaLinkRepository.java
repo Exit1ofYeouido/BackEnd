b05960c34855ca92a6e48bfa1fd2b0fd41f71c94
lemonticsoul <sjho714@naver.com>
2024-08-28 14:43:49 +0900

    feat:jpql로 변경
    #4

--- Application/Reward/src/main/java/com/example/Reward/Advertisement/Repository/MediaLinkRepository.java
+++ Application/Reward/src/main/java/com/example/Reward/Advertisement/Repository/MediaLinkRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 public interface MediaLinkRepository extends JpaRepository<MediaLink,Long> {
 
-    @Query(nativeQuery = true, value = "SELECT * FROM media_link as m WHERE (:enterprises IS NULL OR m.enterprise_name NOT IN (:enterprises)) AND (:medialinks IS NULL OR m.id NOT IN (:medialinks))")
+    @Query("SELECT m FROM MediaLink m WHERE (:enterprises IS NULL OR m.enterpriseName NOT IN :enterprises) AND (:medialinks IS NULL OR m.id NOT IN :medialinks)")
     List<MediaLink> findmedialink(@Param("medialinks") List<Long> mediaLinkIdList,@Param("enterprises") List<String> enterpriseNoneList);
 }
 
