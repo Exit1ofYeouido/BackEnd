5f1abae898e5b6758d0b6170b62f0291bcedc3a0
lemonticsoul <sjho714@naver.com>
2024-08-30 20:58:13 +0900

    feat: swagger bear토큰 설정 추가

--- Application/Reward/src/main/java/com/example/Reward/Config/SwaggerConfig.java
+++ Application/Reward/src/main/java/com/example/Reward/Config/SwaggerConfig.java
@@ -1,5 +1,7 @@
 package com.example.Reward.Config;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
@@ -9,12 +11,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-//@SecurityScheme(
-//        name = "bearerAuth",
-//        type = SecuritySchemeType.HTTP,
-//        bearerFormat = "JWT",
-//        scheme = "bearer"
-//)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
 public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI(@Value("${service-url}") String url) {
