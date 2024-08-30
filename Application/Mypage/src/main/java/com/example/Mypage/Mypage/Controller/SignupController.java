306267e6cbc08a353d626af17a065d9142b7d0e4
Ho-Da-Dak <godqhr721@gmail.com>
2024-08-31 00:28:50 +0900

    refactor : JoinController -> SignupController
    #12

--- Application/Mypage/src/main/java/com/example/Mypage/Mypage/Controller/SignupController.java
+++ Application/Mypage/src/main/java/com/example/Mypage/Mypage/Controller/SignupController.java
@@ -5,6 +5,7 @@ import com.example.Mypage.Mypage.Service.SignupService;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,15 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-public class JoinController {
+public class SignupController {
 
     private final SignupService signupService;
 
     @PostMapping("/signup")
     @Operation(description = "회원가입 API")
-    public ResponseEntity<?> joinUser(@Valid @RequestBody MemberSignupRequestDTO memberSignupRequestDTO) {
-        signupService.saveMember(memberSignupRequestDTO);
-        return ResponseEntity.ok("good");
+    public ResponseEntity<String> joinUser(@Valid @RequestBody MemberSignupRequestDTO memberSignupRequestDTO) {
+        if (signupService.saveMember(memberSignupRequestDTO)) {
+            return ResponseEntity.status(HttpStatus.CREATED).body("회원가입 성공");
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("회원가입 실패");
+
     }
 
 }
