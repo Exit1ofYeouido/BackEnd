3db0c125d7f0c7c48d774a430d32a17db743650a
LimSeHyeon <dlatpgus1205@naver.com>
2024-08-30 14:18:48 +0900

    fix: JPQL 수정
    #3

--- Application/Reward/src/main/java/com/example/Reward/Common/Repository/EventRepository.java
+++ Application/Reward/src/main/java/com/example/Reward/Common/Repository/EventRepository.java
@@ -1,7 +1,6 @@
 package com.example.Reward.Common.Repository;
 
 import com.example.Reward.Common.Entity.Event;
-import com.example.Reward.Receipt.Dto.out.EventDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,7 +11,7 @@ public interface EventRepository extends JpaRepository<Event,Long> {
 
     Event findByEnterpriseNameContaining(String enterprisename);
 
-    List<Event> findByGreaterThanAndContentId(Long rewardAmount, Long contentId);
+    List<Event> findByRewardAmountGreaterThanEqualAndContentId(Long rewardAmount, Long contentId);
 
     @Query(nativeQuery = true, value = "SELECT code FROM event WHERE enterprise_name = ? AND content_id = 2")
     String findCodeByEnterpriseName(@Param("enterpriseName") String enterpriseName);
