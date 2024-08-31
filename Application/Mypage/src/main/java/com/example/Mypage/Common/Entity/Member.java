9d45cdd167279a8529d43476acbb011a5e0c517c
lemonticsoul <sjho714@naver.com>
2024-09-01 08:46:23 +0900

    fix:컬럼명 재통일

--- Application/Mypage/src/main/java/com/example/Mypage/Common/Entity/Member.java
+++ Application/Mypage/src/main/java/com/example/Mypage/Common/Entity/Member.java
@@ -1,6 +1,7 @@
 package com.example.Mypage.Common.Entity;
 
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -16,6 +17,7 @@ public class Member {
 
     @Id
     @GeneratedValue
+    @Column(name="member_id")
     private Long id;
 
     private String name;
