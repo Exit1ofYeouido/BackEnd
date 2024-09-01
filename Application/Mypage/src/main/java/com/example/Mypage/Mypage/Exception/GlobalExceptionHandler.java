fc572afdc76674fea6c9df899c2d004ef80aa84f
Ho-Da-Dak <godqhr721@gmail.com>
2024-09-01 22:55:37 +0900

    fix :  코드 개선 및 Entity 정보 수정
    #12
    Member Entity의 point 필드 이동
    security httpBasic.disable()
    ExceptionHandler 오류내용 로그 출력 추가

--- Application/Mypage/src/main/java/com/example/Mypage/Mypage/Exception/GlobalExceptionHandler.java
+++ Application/Mypage/src/main/java/com/example/Mypage/Mypage/Exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.example.Mypage.Mypage.Exception;
 
 import jakarta.persistence.PessimisticLockException;
 import java.util.stream.Collectors;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(DuplicateIdException.class)
@@ -24,6 +26,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleGeneralException(Exception e) {
+        log.error("서버에러 발생 {} =>", e.getMessage());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body("처리 중 오류가 발생했습니다.");
     }
