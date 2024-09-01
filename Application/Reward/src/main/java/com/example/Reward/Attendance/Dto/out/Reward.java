263ae57dd87e2c2206281b3d37c10ccb0961d9aa
LimSeHyeon <dlatpgus1205@naver.com>
2024-09-01 23:22:46 +0900

    feat: push 안 돼 있던 DTO 파일들 업로드
    #17

---
+++ Application/Reward/src/main/java/com/example/Reward/Attendance/Dto/out/Reward.java
@@ -0,0 +1,10 @@
+package com.example.Reward.Attendance.Dto.out;
+
+import lombok.Getter;
+
+@Getter
+public class Reward {
+    private String code;
+    private String enterpriseName;
+    private double amount;
+}
