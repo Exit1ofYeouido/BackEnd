975a429edc57fe11d3d566516b9251d3c5afdf21
LimSeHyeon <dlatpgus1205@naver.com>
2024-09-02 17:15:29 +0900

    update: 예외처리 사항 추가
    #23

--- Application/Reward/src/main/java/com/example/Reward/Receipt/Exception/ReceiptExceptions/MissingOcrInfoException.java
+++ Application/Reward/src/main/java/com/example/Reward/Receipt/Exception/ReceiptExceptions/MissingOcrInfoException.java
@@ -5,9 +5,11 @@ import com.example.Reward.Receipt.Exception.ReceiptException;
 
 public class MissingOcrInfoException extends ReceiptException {
     private String url;
+    private String missingValue;
 
-    public MissingOcrInfoException(String url) {
+    public MissingOcrInfoException(String missingValue, String url) {
         super(ReceiptErrorStatus.MISSING_OCR_INFO);
         this.url = url;
+        this.missingValue = missingValue;
     }
 }
