8c514221a8025a27cad88a2a2f05443057887b3d
LimSeHyeon <dlatpgus1205@naver.com>
2024-09-03 00:51:24 +0900

    fix: @ExceptionHandler 어노테이션 추가
    #23

--- Application/Reward/src/main/java/com/example/Reward/Receipt/Exception/ReceiptExceptionHandler.java
+++ Application/Reward/src/main/java/com/example/Reward/Receipt/Exception/ReceiptExceptionHandler.java
@@ -3,11 +3,13 @@ package com.example.Reward.Receipt.Exception;
 import com.example.Reward.Receipt.Controller.ReceiptController;
 import com.example.Reward.Receipt.Exception.ReceiptExceptions.*;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice(basePackageClasses = ReceiptController.class)
 public class ReceiptExceptionHandler {
 
+    @ExceptionHandler(GiveStockErrorException.class)
     private ResponseEntity<ReceiptErrorResponse> handler(GiveStockErrorException e) {
         ReceiptErrorResponse response = ReceiptErrorResponse.builder()
                 .status(e.getStatus())
@@ -16,6 +18,7 @@ public class ReceiptExceptionHandler {
         return ResponseEntity.badRequest().body(response);
     }
 
+    @ExceptionHandler(KISApiException.class)
     private ResponseEntity<ReceiptErrorResponse> handler(KISApiException e) {
         ReceiptErrorResponse response = ReceiptErrorResponse.builder()
                 .status(e.getStatus())
@@ -24,6 +27,7 @@ public class ReceiptExceptionHandler {
         return ResponseEntity.badRequest().body(response);
     }
 
+    @ExceptionHandler(S3UploadFailedException.class)
     private ResponseEntity<ReceiptErrorResponse> handler(S3UploadFailedException e) {
         ReceiptErrorResponse response = ReceiptErrorResponse.builder()
                 .status(e.getStatus())
@@ -32,6 +36,7 @@ public class ReceiptExceptionHandler {
         return ResponseEntity.badRequest().body(response);
     }
 
+    @ExceptionHandler(NoStockException.class)
     private ResponseEntity<ReceiptErrorResponse> handler(NoStockException e) {
         ReceiptErrorResponse response = ReceiptErrorResponse.builder()
                 .status(e.getStatus())
@@ -40,6 +45,7 @@ public class ReceiptExceptionHandler {
         return ResponseEntity.badRequest().body(response);
     }
 
+    @ExceptionHandler(StockNotFoundException.class)
     private ResponseEntity<ReceiptErrorResponse> handler(StockNotFoundException e) {
         ReceiptErrorResponse response = ReceiptErrorResponse.builder()
                 .status(e.getStatus())
@@ -49,6 +55,7 @@ public class ReceiptExceptionHandler {
         return ResponseEntity.badRequest().body(response);
     }
 
+    @ExceptionHandler(OcrErrorException.class)
     private ResponseEntity<ReceiptErrorResponse> handler(OcrErrorException e) {
         ReceiptErrorResponse response = ReceiptErrorResponse.builder()
                 .status(e.getStatus())
@@ -58,6 +65,7 @@ public class ReceiptExceptionHandler {
         return ResponseEntity.badRequest().body(response);
     }
 
+    @ExceptionHandler(InvalidFileExtensionException.class)
     private ResponseEntity<ReceiptErrorResponse> handler(InvalidFileExtensionException e) {
         ReceiptErrorResponse response = ReceiptErrorResponse.builder()
                 .status(e.getStatus())
@@ -67,6 +75,7 @@ public class ReceiptExceptionHandler {
         return ResponseEntity.badRequest().body(response);
     }
 
+    @ExceptionHandler(MissingOcrInfoException.class)
     private ResponseEntity<ReceiptErrorResponse> handler(MissingOcrInfoException e) {
         ReceiptErrorResponse response = ReceiptErrorResponse.builder()
                 .status(e.getStatus())
