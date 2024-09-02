c5f7633e3249eafe684e4b6e985222b6b500b0fa
LimSeHyeon <dlatpgus1205@naver.com>
2024-09-02 17:11:21 +0900

    rename: 오타 수정
    #23

--- Application/Reward/src/main/java/com/example/Reward/Receipt/Exception/ReceiptExceptions/MissingOcrInfoException.java
+++ Application/Reward/src/main/java/com/example/Reward/Receipt/Exception/ReceiptExceptions/MissingOcrInfoException.java
@@ -3,10 +3,10 @@ package com.example.Reward.Receipt.Exception.ReceiptExceptions;
 import com.example.Reward.Receipt.Exception.ReceiptErrorStatus;
 import com.example.Reward.Receipt.Exception.ReceiptException;
 
-public class MissintOcrInfoException extends ReceiptException {
+public class MissingOcrInfoException extends ReceiptException {
     private String url;
 
-    public MissintOcrInfoException(String url) {
+    public MissingOcrInfoException(String url) {
         super(ReceiptErrorStatus.MISSING_OCR_INFO);
         this.url = url;
     }
