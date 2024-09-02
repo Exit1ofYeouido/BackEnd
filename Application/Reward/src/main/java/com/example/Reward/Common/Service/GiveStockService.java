1b976af21237ce8b9d9d69b5a43fe212097c47f7
LimSeHyeon <dlatpgus1205@naver.com>
2024-09-03 01:00:02 +0900

    update: 한국투자증권 API 요청 예외처리
    #23

--- Application/Reward/src/main/java/com/example/Reward/Common/Service/GiveStockService.java
+++ Application/Reward/src/main/java/com/example/Reward/Common/Service/GiveStockService.java
@@ -5,6 +5,7 @@ import com.example.Reward.Common.Kafka.GiveStockProduceDto;
 import com.example.Reward.Common.Repository.EventRepository;
 import com.example.Reward.Receipt.Dto.in.RewardRequestDTO;
 import com.example.Reward.Receipt.Dto.webClient.PresentPriceDTO;
+import com.example.Reward.Receipt.Exception.ReceiptExceptions.KISApiException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -27,17 +28,21 @@ public class GiveStockService {
     private static final String STOCK_BASE_URL = "https://openapi.koreainvestment.com:9443";
 
     public Integer getPrice(String enterpriseName) {
-        String stockCode = eventRepository.findByEnterpriseNameContainingAndContentId(enterpriseName, 2L).getStockCode();
-        Mono<PresentPriceDTO> response = webClient.get()
-                .uri(STOCK_BASE_URL + "/uapi/domestic-stock/v1/quotations/inquire-price?FID_COND_MRKT_DIV_CODE=J&FID_INPUT_ISCD={param}", stockCode)
-                .header("authorization", "Bearer " + generatedToken.getAccessToken())
-                .header("appkey", appKey)
-                .header("appsecret", appSecret)
-                .header("tr_id", "FHKST01010100")
-                .retrieve()
-                .bodyToMono(PresentPriceDTO.class);
-        PresentPriceDTO result = response.block();
-        return result.getOutput().getStck_prpr();
+        try {
+            String stockCode = eventRepository.findByEnterpriseNameContainingAndContentId(enterpriseName, 2L).getStockCode();
+            Mono<PresentPriceDTO> response = webClient.get()
+                    .uri(STOCK_BASE_URL + "/uapi/domestic-stock/v1/quotations/inquire-price?FID_COND_MRKT_DIV_CODE=J&FID_INPUT_ISCD={param}", stockCode)
+                    .header("authorization", "Bearer " + generatedToken.getAccessToken())
+                    .header("appkey", appKey)
+                    .header("appsecret", appSecret)
+                    .header("tr_id", "FHKST01010100")
+                    .retrieve()
+                    .bodyToMono(PresentPriceDTO.class);
+            PresentPriceDTO result = response.block();
+            return result.getOutput().getStck_prpr();
+        } catch (Exception e) {
+            throw new KISApiException();
+        }
     }
 
     public Double calDecimalStock(Integer priceOfStock) {
