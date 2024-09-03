05fc3eb061f4fbe862250386ad97a3f76203ecbe
Ho-Da-Dak <godqhr721@gmail.com>
2024-09-03 23:52:09 +0900

    feat : 판매완료 주식 entity 추가
    #27

---
+++ Application/Mypage/src/main/java/com/example/Mypage/Common/Entity/CompletedStockSale.java
@@ -0,0 +1,34 @@
+package com.example.Mypage.Common.Entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class CompletedStockSale {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "stock_sale_id")
+    private int id;
+
+    private String stockCode;
+
+    private String enterpriseName;
+
+    private double amount;
+
+    private int price;
+
+    private LocalDateTime soldTime;
+}
---
+++ Application/Mypage/src/main/java/com/example/Mypage/Common/Repository/CompletedStockSaleRepository.java
@@ -0,0 +1,7 @@
+package com.example.Mypage.Common.Repository;
+
+import com.example.Mypage.Common.Entity.CompletedStockSale;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CompletedStockSaleRepository extends JpaRepository<CompletedStockSale, Long> {
+}
