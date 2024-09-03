268be54a9a4008f8fac193478f14f65a1508ee9c
lemonticsoul <sjho714@naver.com>
2024-09-03 10:26:38 +0900

    feat: swagger추가

--- Application/Mypage/src/main/java/com/example/Mypage/Config/SwaggerConfig.java
+++ Application/Mypage/src/main/java/com/example/Mypage/Config/SwaggerConfig.java
@@ -1,5 +1,7 @@
 package com.example.Mypage.Config;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
@@ -9,20 +11,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-//@SecurityScheme(
-//        name = "bearerAuth",
-//        type = SecuritySchemeType.HTTP,
-//        bearerFormat = "JWT",
-//        scheme = "bearer"
-//)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
 public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI(@Value("${service-url}") String url) {
 
-//        List<Tag> tagList = List.of(
-//                createTag("기본 API", "기본임 ㅇㅇ 여기에다가 태그 여러개 붙여놓으셈"),
-//                createTag("기본 2","기본이 두개 ~")
-//                );
+
         return new OpenAPI()
                 .addServersItem(new Server().url(url))
                 .info(new Info()
