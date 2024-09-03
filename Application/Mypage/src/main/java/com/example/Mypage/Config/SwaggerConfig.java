2ebdebce3a43181e777fa95ef093e780dedb6466
lemonticsoul <sjho714@naver.com>
2024-09-03 10:33:37 +0900

    fix:swagger 수정

--- Application/Mypage/src/main/java/com/example/Mypage/Config/SwaggerConfig.java
+++ Application/Mypage/src/main/java/com/example/Mypage/Config/SwaggerConfig.java
@@ -2,33 +2,36 @@ package com.example.Mypage.Config;
 
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.servers.Server;
 import io.swagger.v3.oas.models.tags.Tag;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Arrays;
+
 @Configuration
-@SecurityScheme(
-        name = "bearerAuth",
-        type = SecuritySchemeType.HTTP,
-        bearerFormat = "JWT",
-        scheme = "bearer"
-)
+
 public class SwaggerConfig {
+
+    io.swagger.v3.oas.models.security.SecurityScheme securityScheme = new io.swagger.v3.oas.models.security.SecurityScheme()
+            .type(io.swagger.v3.oas.models.security.SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+            .in(io.swagger.v3.oas.models.security.SecurityScheme.In.HEADER).name("Authorization");
+    SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
     @Bean
     public OpenAPI openAPI(@Value("${service-url}") String url) {
 
 
         return new OpenAPI()
-                .addServersItem(new Server().url(url))
-                .info(new Info()
-                        .title("기본 API")
-                        .version("1.0")
-                        .description("api 명세서 한번 보쉴?"));
-//                        ).tags(tagList);
+                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+                .security(Arrays.asList(securityRequirement))
+                .info(new Info().title("마이페이지 API명세서")
+                        .version("1.0.0")
+                );
     }
 
     private Tag createTag(String name, String description) {
