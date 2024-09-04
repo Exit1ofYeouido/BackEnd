03f39c6e95097524301b98ac3c8cce9ae0037ea9
lemonticsoul <sjho714@naver.com>
2024-09-04 16:51:53 +0900

    init:Controller 생성

---
+++ Application/Search/src/main/java/com/example/Search/Log/Controller/LogController.java
@@ -0,0 +1,13 @@
+package com.example.Search.Log.Controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Tag(name="Log API")
+@RequestMapping("admin")
+public class LogController {
+}
