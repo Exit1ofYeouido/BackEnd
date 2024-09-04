c2705759c437612423f111d986d4f9eabc289350
lemonticsoul <sjho714@naver.com>
2024-09-04 16:53:35 +0900

    init:Controller 생성

--- Application/Search/src/main/java/com/example/Search/Log/Controller/LogController.java
+++ Application/Search/src/main/java/com/example/Search/Log/Controller/LogController.java
@@ -1,7 +1,10 @@
 package com.example.Search.Log.Controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name="Log API")
 @RequestMapping("admin")
 public class LogController {
+
+
+
+    @GetMapping("search-history/stock")
+    @Operation(description = "주식 종목별 검색량 조회")
+    public ResponseEntity<?> gethistoryStock
+
+
 }
