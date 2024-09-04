4ce88319f42bbde15dc8e2d2080685f9df77e34b
lemonticsoul <sjho714@naver.com>
2024-09-04 16:55:16 +0900

    init:초기설정

--- Application/Search/src/main/java/com/example/Search/Log/Service/LogService.java
+++ Application/Search/src/main/java/com/example/Search/Log/Service/LogService.java
@@ -1,4 +1,10 @@
 package com.example.Search.Log.Service;
 
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class LogService {
 }
