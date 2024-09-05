5979de62ac82de8f7b07ab2ea4465c2d7d57986d
LimSeHyeon <dlatpgus1205@naver.com>
2024-09-05 23:32:48 +0900

    update: 생성자 추가
    #45

--- Application/Search/src/main/java/com/example/Search/Common/Entity/Stock.java
+++ Application/Search/src/main/java/com/example/Search/Common/Entity/Stock.java
@@ -1,11 +1,15 @@
 package com.example.Search.Common.Entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
 @Table
+@NoArgsConstructor
+@AllArgsConstructor
 public class Stock {
 
     @Id
