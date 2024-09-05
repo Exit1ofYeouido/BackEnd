8736db5423d7fc15c9a5528c85bf34a45a9083ba
lemonticsoul <127959482+lemonticsoul@users.noreply.github.com>
2024-09-05 17:55:00 +0900

    feat:token_info 수정

--- Application/Mypage/src/main/java/com/example/Mypage/Mypage/Webclient/Service/GeneratedToken.java
+++ Application/Mypage/src/main/java/com/example/Mypage/Mypage/Webclient/Service/GeneratedToken.java
@@ -13,6 +13,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
+import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
@@ -32,19 +33,19 @@ public class GeneratedToken {
 
     public String getAccessToken() {
 
-        List<TokenInfo> tokenInfos=tokenInfoRepository.findAll();
+        Optional<TokenInfo> tokenInfos=tokenInfoRepository.findById(1L);
 
         if (tokenInfos.isEmpty()) {
             ACCESS_TOKEN = generateAccessToken();
             TokenInfo tokenInfo = TokenInfo
                     .builder()
+                    .id(1L)
                     .accessToken(ACCESS_TOKEN)
                     .build();
-
             tokenInfoRepository.save(tokenInfo);
             return ACCESS_TOKEN;
         }
-        String Is_ACCESS_TOKEN = tokenInfos.get(0).getAccessToken();
+        String Is_ACCESS_TOKEN = tokenInfos.get().getAccessToken();
 
         return Is_ACCESS_TOKEN;
     }
