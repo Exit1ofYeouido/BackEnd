b477ff6bd9dfdaf52ba352a77783a6eca1e16353
LimSeHyeon <dlatpgus1205@naver.com>
2024-09-05 13:32:14 +0900

    fix: Entity에 기본 생성자 추가
    #43

--- Application/Reward/src/main/java/com/example/Reward/Attendance/Entity/Attendance.java
+++ Application/Reward/src/main/java/com/example/Reward/Attendance/Entity/Attendance.java
@@ -5,10 +5,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
+@NoArgsConstructor
 public class Attendance {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
