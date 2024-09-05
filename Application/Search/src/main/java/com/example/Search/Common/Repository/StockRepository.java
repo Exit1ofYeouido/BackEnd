e53ed6d28cc05248036445efc635e7ea7e990069
LimSeHyeon <dlatpgus1205@naver.com>
2024-09-05 23:33:46 +0900

    update: count -> amount 변경, 레파지토리 메소드 추가
    #45

--- Application/Search/src/main/java/com/example/Search/Common/Repository/StockRepository.java
+++ Application/Search/src/main/java/com/example/Search/Common/Repository/StockRepository.java
@@ -17,4 +17,6 @@ public interface StockRepository extends JpaRepository<Stock,String> {
 
     @Query(value = "SELECT s FROM Stock s ORDER BY RAND()")
     List<Stock> findRandomStocks(Pageable pageable);
+
+    Stock findByCode(String code);
 }
