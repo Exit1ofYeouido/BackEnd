398ac3408b573635a6985c527df558e96bb03978
Ho-Da-Dak <godqhr721@gmail.com>
2024-09-07 20:31:35 +0900

    refactor : 계좌조회 예외처리 핸들러 수정
    #70

--- Application/Mypage/src/main/java/com/example/Mypage/Mypage/Exception/GlobalExceptionHandler.java
+++ Application/Mypage/src/main/java/com/example/Mypage/Mypage/Exception/GlobalExceptionHandler.java
@@ -39,9 +39,13 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(AccountNotFoundException.class)
-    public ResponseEntity<String> handleAccountNotFoundException(AccountNotFoundException e) {
+    public ResponseEntity<ErrorResponseDto> handleAccountNotFoundException(AccountNotFoundException e) {
         log.error(e.getMessage());
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        ErrorResponseDto errorResponseDto = ErrorResponseDto.builder()
+                .message(e.getMessage())
+                .time(LocalDateTime.now())
+                .build();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponseDto);
     }
 
     @ExceptionHandler(MemberNotFoundException.class)
