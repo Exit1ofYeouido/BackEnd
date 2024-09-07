2e40ae9a4cac3157569009bb7a5b8bc6d6cc09ec
Ho-Da-Dak <godqhr721@gmail.com>
2024-09-08 01:46:57 +0900

    feat : 토큰 갱신 로직 추가
    #75

--- Application/Search/src/main/java/com/example/Search/Search/Api/GeneratedToken.java
+++ Application/Search/src/main/java/com/example/Search/Search/Api/GeneratedToken.java
@@ -5,14 +5,12 @@ import com.example.Search.Common.Entity.Token;
 import com.example.Search.Common.Entity.TokenInfo;
 import com.example.Search.Common.Repository.TokenInfoRepository;
 import com.example.Search.Search.SearchDTO.OauthInfoDTO;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
-
-import java.util.List;
-import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
@@ -21,25 +19,31 @@ public class GeneratedToken {
     private final TokenInfoRepository tokenInfoRepository;
 
     @Value("${app.key}")
-    private String APPKEY ;
+    private String APPKEY;
 
     @Value("${app.secret}")
-    private String APPSECRET ;
+    private String APPSECRET;
+
+    @Value("${app.fourth-key}")
+    private String FOURTH_API_KEY;
+
+    @Value("${app.fourth-secret}")
+    private String FOURTH_API_SECRET;
 
     public static String ACCESS_TOKEN;
 
-    WebClient client=WebClient.create();
+    WebClient client = WebClient.create();
 
-    public String getAccessToken() {
+    public String getAccessToken(Long tokenId) {
 
-        Optional<TokenInfo> tokenInfos=tokenInfoRepository.findById(1L);
+        Optional<TokenInfo> tokenInfos = tokenInfoRepository.findById(tokenId);
 
         if (tokenInfos.isEmpty()) {
             ACCESS_TOKEN = generateAccessToken();
 
             TokenInfo tokenInfo = TokenInfo
                     .builder()
-                    .id(1L)
+                    .id(tokenId)
                     .accessToken(ACCESS_TOKEN)
                     .build();
 
@@ -52,9 +56,9 @@ public class GeneratedToken {
         return Is_ACCESS_TOKEN;
     }
 
-    public String generateAccessToken(){
+    public String generateAccessToken() {
 
-        String url = "https://openapi.koreainvestment.com:9443" + "/oauth2/tokenP";
+        String url = "https://openapi.koreainvestment.com:9443/oauth2/tokenP";
         OauthInfoDTO bodyOauthInfoDto = OauthInfoDTO.builder()
                 .grant_type("client_credentials")
                 .appkey(APPKEY)
@@ -76,6 +80,29 @@ public class GeneratedToken {
         ACCESS_TOKEN = token.getAccess_token();
 
         return ACCESS_TOKEN;
+    }
 
+    public String generateFourthAccessToken() {
+
+        String url = "https://openapi.koreainvestment.com:9443" + "/oauth2/tokenP";
+        OauthInfoDTO bodyOauthInfoDto = OauthInfoDTO.builder()
+                .grant_type("client_credentials")
+                .appkey(FOURTH_API_KEY)
+                .appsecret(FOURTH_API_SECRET)
+                .build();
+
+        Mono<Token> mono = client.post()
+                .uri(url)
+                .header("content-type", "application/json")
+                .bodyValue(bodyOauthInfoDto)
+                .retrieve()
+                .bodyToMono(Token.class);
+
+        Token token = mono.block();
+        if (token == null) {
+            throw new RuntimeException("액세스 토큰을 가져올 수 없습니다.");
+        }
+
+        return ACCESS_TOKEN = token.getAccess_token();
     }
 }
