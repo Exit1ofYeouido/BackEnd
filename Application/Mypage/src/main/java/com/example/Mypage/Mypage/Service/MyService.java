bb2adb10e38034a9d1ed61c011b6df760fd1c723
lemonticsoul <127959482+lemonticsoul@users.noreply.github.com>
2024-09-09 10:07:17 +0900

    feat: 주식 수량이 0이면 내 주식 안보이게 설정

--- Application/Mypage/src/main/java/com/example/Mypage/Mypage/Service/MyService.java
+++ Application/Mypage/src/main/java/com/example/Mypage/Mypage/Service/MyService.java
@@ -103,6 +103,9 @@ public class MyService {
 
         for (MemberStock memberStock : memberStocks) {
             double stockCount = memberStock.getAmount();
+            if (stockCount ==0){
+                continue;
+            }
             int stockPrice = memberStock.getAveragePrice();
             int currentPrice = apiService.getPrice(memberStock.getStockCode());
 
